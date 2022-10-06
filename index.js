0d0b241bfbbf8453d6cf1c995da15e82bed96160
karagodini <karagodini@yandex.ru>
2022-10-06 17:57:51 +0300

    portfolio

--- index.js
+++ index.js
@@ -15,38 +15,20 @@ for (let anchor of anchors) {
 }
 
 
-//Всплывающее окно
-let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
-let popupBg2 = document.querySelector('.popup__bg2'); // Фон попап окна
-let popup = document.querySelector('.popup'); // Само окно
-let popup2 = document.querySelector('.popup2'); // Само окно
-let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
-let openPopupButtons2 = document.querySelectorAll('.open-popup2'); // Кнопки для показа окна
-let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
-openPopupButtons.forEach((button) => { // Перебираем все кнопки
-  button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
-      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-      popupBg.classList.add('active'); // Добавляем класс 'active' для фона
-      popup.classList.add('active'); // И для самого окна
-  })
-});
-
-closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
-  popupBg.classList.remove('active'); // Убираем активный класс с фона
-  popup.classList.remove('active'); // И с окна
-});
-
-/**/
-let okno1 = document.querySelector('.okno1'),
-		popup1 = document.querySelector('.popup1'),
+/*Модальное окно*/
+let popup_basic = document.querySelector('.popup-basic'),
+		popup_basic_open = document.querySelector('.popup-basic_open'),
+    popup_basic_bg = document.querySelector('.popup-basic_bg'),
 		close = document.querySelector('.close');
 
-    popup1.addEventListener('click', (e) => {
+    popup_basic_open.addEventListener('click', (e) => {
     		e.preventDefault();
-        okno1.classList.remove('okno1');
-        okno1.classList.add('active');
+        popup_basic.classList.remove('popup-basic');
+        popup_basic.classList.add('okno-active');
+        popup_basic_bg.classList.add('popup-basic_bg-active');
     })
 
     close.addEventListener('click',() => {
-        okno1.classList.remove('active');
+        popup_basic.classList.remove('okno-active');
+        popup_basic_bg.classList.remove('popup-basic_bg-active');
     });
